0188ebf3a882bcb87d592f5278488c6466c93348
thomaskippster <deine.email@adresse.de>
2026-03-06 20:12:28 +0100

    Rename RetrofitClient to EbayRetrofitClient and finalize eBay API setup in api package

--- app/src/main/kotlin/com/example/ebayquicksale/api/EbayRetrofitClient.kt
+++ app/src/main/kotlin/com/example/ebayquicksale/api/EbayRetrofitClient.kt
@@ -3,7 +3,7 @@ package com.example.ebayquicksale.api
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitClient {
+object EbayRetrofitClient {
     private const val BASE_URL = "https://api.ebay.com/"
 
     val ebayApiService: EbayApiService by lazy {
